c47225c66c16dbd603e4cdc8c4469c4d504bdf31
heunnajo <61869540+heunnajo@users.noreply.github.com>
2021-07-19 18:52:58 +0900

    Find Order4. JPQL and DTO by JPA.

--- src/main/java/jpabook_v2/jpashop_v2/api/OrderSimpleApiController.java
+++ src/main/java/jpabook_v2/jpashop_v2/api/OrderSimpleApiController.java
@@ -5,6 +5,7 @@ import jpabook_v2.jpashop_v2.domain.Order;
 import jpabook_v2.jpashop_v2.domain.OrderStatus;
 import jpabook_v2.jpashop_v2.repository.OrderRepository;
 import jpabook_v2.jpashop_v2.repository.OrderSearch;
+import jpabook_v2.jpashop_v2.repository.order.simplequery.OrderSimpleQueryRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.*;
 
@@ -30,7 +30,7 @@ import static java.util.stream.Collectors.*;
 public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
-
+    private final OrderSimpleQueryRepository orderSimpleQueryRepository;
 //    @GetMapping("/api/v1/simple-orders")
 //    public List<Order> ordersV1(){//EAGER로 변경시 : findAllByString : JPQL은 그대로 SQL로 번역. =>Order만 가져오는 쿼리인데 EAGER로 되있으면 연관된 엔티티 모두 다 조회한다!!(N+1문제 발생), 다른 API에서 불필요한 데이터들도 끌고 온다!
 //        List<Order> all = orderRepository.findAllByCriteria(new OrderSearch());//List<Order>
@@ -61,19 +61,24 @@ public class OrderSimpleApiController {
     }
 
     @Data
-    static class SimpleOrderDto {
+    static class SimpleOrderDto{
         private Long orderId;
         private String name;
         private LocalDateTime orderDate; //주문시간 private OrderStatus orderStatus;
         private OrderStatus orderStatus;
         private Address address;
         public SimpleOrderDto(Order order) {
-            orderId = order.getId();
-            name = order.getMember().getName();//LAZY 초기화! 영속성 컨텍스트 조회해서 없으면 DB MEMBER 조회 쿼리 날린다!
-            orderDate = order.getOrderDate();
-            orderStatus = order.getStatus();
-            address = order.getDelivery().getAddress();//LAZY 초기화! 영속성 컨텍스트 조회해서 없으면 DB DELIVERY 조회 쿼리 날린다!
+            this.orderId = orderId;
+            this.name = name;
+            this.orderDate = orderDate;
+            this.orderStatus = orderStatus;
+            this.address = address;
         }
+    }
+
+    @GetMapping("/api/v4/simple-orders")
+    public List<OrderSimpleQueryDto> ordersV4() {
+        return orderSimpleQueryRepository.findOrderDtos();
     }
 
 }
