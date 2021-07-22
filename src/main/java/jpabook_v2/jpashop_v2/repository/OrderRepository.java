e891b8609d52ea7fe7295c50e17902bc0e583d16
heunnajo <61869540+heunnajo@users.noreply.github.com>
2021-07-22 14:39:31 +0900

    Get Collection data using DTO and fetch join - Paging and Optimization
    xToOne : Use fetch join
    Others : Use batch_size

--- src/main/java/jpabook_v2/jpashop_v2/repository/OrderRepository.java
+++ src/main/java/jpabook_v2/jpashop_v2/repository/OrderRepository.java
@@ -70,60 +70,14 @@ public class OrderRepository {
                         " join fetch oi.item i", Order.class)//order와 orderItems를 조인! ex) 주문 한건에 주문상품 4개면 조인하면 결과는 4개가 된다.
                 .getResultList();
     }
+
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+        return em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d", Order.class)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
 }
-/*
-public class OrderRepository {
-    private final EntityManager em;
-
-    public void save(Order order){
-        em.persist(order);
-    }
-
-    public Order findOne(Long id){
-        return em.find(Order.class,id);
-    }
-
-    //검색 기능
-    public List<Order> findAll(OrderSearch orderSearch){
-        //order를 먼저 조회하고, member와 조인.
-        //order와 연관된 member 조인한다.
-
-        //동적 쿼리 작성 방법1.
-        String jpql = "select o from Order o join o.member m";
-        boolean isFirstCondition = true;
-
-        //JPQL 동적 빌드
-        //주문 상태 검색
-        if(orderSearch.getOrderStatus() != null){
-            if(isFirstCondition){
-                jpql += "where";
-                isFirstCondition = false;
-            } else {
-                jpql += "and";
-            }
-            jpql += "o.status = :status";
-        }
-        //회원 이름
-        if(StringUtils.hasText(orderSearch.getMemberName())){
-            if(isFirstCondition){
-                jpql += "where";
-                isFirstCondition = false;
-            } else {
-                jpql += "and";
-            }
-            jpql += "o.status = :status";
-        }
-
-        TypedQuery<Order> query = em.createQuery(jpql, Order.class)
-                .setMaxResults(1000);
-
-        //동적 파라미터 바인딩
-        if(orderSearch.getOrderStatus() != null){
-            query = query.setParameter("status",orderSearch.getOrderStatus());
-        }
-        if(StringUtils.hasText(orderSearch.getMemberName())){
-            query = query.setParameter("name",orderSearch.getMemberName());
-        }
-        return query.getResultList();
-    }
-}*/
