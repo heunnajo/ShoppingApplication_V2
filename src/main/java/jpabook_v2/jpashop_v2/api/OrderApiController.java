7f4d98ace06d8cd2ac4d16dedadc0f70dbda6cf8
heunnajo <61869540+heunnajo@users.noreply.github.com>
2021-07-23 10:03:20 +0900

    Turn off the OSIV to save DB connection resources.
    Create new service package of Query for API and a separate service for API(OrderQueryService).
    Copy and paste Related DTO under the Service > Query.

--- src/main/java/jpabook_v2/jpashop_v2/api/OrderApiController.java
+++ src/main/java/jpabook_v2/jpashop_v2/api/OrderApiController.java
@@ -10,6 +10,7 @@ import jpabook_v2.jpashop_v2.repository.order.query.OrderFlatDto;
 import jpabook_v2.jpashop_v2.repository.order.query.OrderItemQueryDto;
 import jpabook_v2.jpashop_v2.repository.order.query.OrderQueryDto;
 import jpabook_v2.jpashop_v2.repository.order.query.OrderQueryRepository;
+import jpabook_v2.jpashop_v2.service.query.OrderQueryService;
 import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -53,6 +54,12 @@ public class OrderApiController {
                 .collect(Collectors.toList());
         return result;
     }
+    private final OrderQueryService orderQueryService;
+    @GetMapping("/api/v3/orders")
+    public List<OrderDto> ordersV33() {
+        return orderQueryService.ordersV3();
+    }
+
     @GetMapping("/api/v3/orders")
     public List<OrderDto> ordersV3() {
         List<Order> orders = orderRepository.findAllWithItem();
@@ -86,17 +93,17 @@ public class OrderApiController {
     public List<OrderQueryDto> ordersV5(){
         return orderQueryRepository.findAllByDto_optimization();
     }
-    @GetMapping("/api/v6/orders")
-    public List<OrderQueryDto> ordersV6() {
-        List<OrderFlatDto> flats = orderQueryRepository.findAllByDto_flat();
-
-        return flats.stream()
-                .collect(groupingBy(o -> new OrderQueryDto(o.getOrderId(), o.getName(), o.getOrderDate(), o.getOrderStatus(), o.getAddress()),
-                        mapping(o -> new OrderItemQueryDto(o.getOrderId(), o.getItemName(), o.getOrderPrice(), o.getCount()), toList())
-                )).entrySet().stream()
-                .map(e -> new OrderQueryDto(e.getKey().getOrderId(), e.getKey().getName(), e.getKey().getOrderDate(), e.getKey().getOrderStatus(), e.getKey().getAddress(), e.getValue()))
-                .collect(toList());
-    }
+//    @GetMapping("/api/v6/orders")
+//    public List<OrderQueryDto> ordersV6() {
+//        List<OrderFlatDto> flats = orderQueryRepository.findAllByDto_flat();
+//
+//        return flats.stream()
+//                .collect(groupingBy(o -> new OrderQueryDto(o.getOrderId(), o.getName(), o.getOrderDate(), o.getOrderStatus(), o.getAddress()),
+//                        mapping(o -> new OrderItemQueryDto(o.getOrderId(), o.getItemName(), o.getOrderPrice(), o.getCount()), toList())
+//                )).entrySet().stream()
+//                .map(e -> new OrderQueryDto(e.getKey().getOrderId(), e.getKey().getName(), e.getKey().getOrderDate(), e.getKey().getOrderStatus(), e.getKey().getAddress(), e.getValue()))
+//                .collect(toList());
+//    }
     @Data
     static class OrderDto{
         private Long orderId;
